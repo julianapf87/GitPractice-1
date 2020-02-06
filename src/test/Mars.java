05ddf7598844f7da5f2366520c98fb8327a39ac3
julianapf87 <56983603+julianapf87@users.noreply.github.com>
2020-02-06 11:54:37 -0500

    Update Mars.java

--- src/test/Mars.java
+++ src/test/Mars.java
@@ -2,17 +2,4 @@ package test;
 
 public class Mars {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-
-		
-		
-		
-		
-	System.out.println("Hello");	
-		
-		
-		
-	}
-
 }
